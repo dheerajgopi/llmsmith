8a2d92f4896250a3d6b3467202b03b62b384de8f
Dheeraj Gopinath <dheerajgopinath@gmail.com>
2024-04-15 12:03:27 +0530

    Docs: Update Sphinx RTD theme options

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,3 +31,8 @@ exclude_patterns = []
 
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
+html_theme_options = {
+    'display_version': True,
+    # Toc options
+    'navigation_depth': 4
+}
